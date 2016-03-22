a1b86734527784ebeff26161084b4142469e23e8
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-22 15:12:43 -0700

    respond with mesages that were previously sent

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,29 +11,28 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
-
+global.messages = [];
 var requestHandler = function(request, response) {
 
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
   var statusCode = 200;
-  var headers = defaultCorsHeaders;
-  var results = [];
-  
-  if (request.method === 'GET' ) { //&& request.url === '/classes/messages') {
+  var headers = defaultCorsHeaders;  
+  if (request.method === 'GET' && request.url === '/classes/messages') {
     headers['Content-Type'] = 'application/json';
-    response.writeHead(response.statusCode, response.headers);
-    response.end(JSON.stringify({results: []}));
+    response.writeHead(statusCode, headers);
+    response.end(JSON.stringify({results: global.messages}));
   }
 
-  if (request.method === 'POST' ) { //&& request.url === '/classes/messages') {
-    response.statusCode = 201;
-    var body = '';
+  if (request.method === 'POST' && request.url === '/classes/messages') {
+    statusCode = 201;
+    var data = '';
     request.on('data', function(chunk) {
+      data += chunk;
     }).on('end', function() {
-      body += chunk;
-      var data = JSON.parse(body);
-      response.writeHead(response.statusCode, response.headers);
-      response.end(data);
+      var post = JSON.parse(data);
+      global.messages.push(post);
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify(post));
     });
   }
 };
@@ -42,6 +41,7 @@ var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
+  'content-type': 'application/json',
   'access-control-max-age': 10 // Seconds.
 };
 
